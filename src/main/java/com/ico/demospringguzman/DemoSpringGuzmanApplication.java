3ef708c9455766a25f0c257f55cd4d8ff3a3e3da
Jose Garcia <josegarcia19809@gmail.com>
2024-01-10 14:18:11 -0600

    Commit inicial

---
+++ src/main/java/com/ico/demospringguzman/DemoSpringGuzmanApplication.java
@@ -0,0 +1,13 @@
+package com.ico.demospringguzman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringGuzmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringGuzmanApplication.class, args);
+	}
+
+}
