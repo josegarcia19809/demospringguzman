c9523743f5d50d0b769373f9476c0335184975b8
Jose Garcia <josegarcia19809@gmail.com>
2024-01-10 14:28:57 -0600

    Pasar datos a la vista

--- src/main/java/com/ico/demospringguzman/controllers/UserController.java
+++ src/main/java/com/ico/demospringguzman/controllers/UserController.java
@@ -1,14 +1,17 @@
 package com.ico.demospringguzman.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
-
+    public String details(Model model){
+        model.addAttribute("title", "Hola Mundo Sprint Boot");
+        model.addAttribute("name", "José");
+        model.addAttribute("lastname", "García");
         return "details";
     }
 }
