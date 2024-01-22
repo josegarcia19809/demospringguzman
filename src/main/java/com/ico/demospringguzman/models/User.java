ca4af7851dc086113898c79fccf8c4d269ac4031
josegarcia19809 <josegarcia19809@gmail.com>
2024-01-21 20:04:14 -0600

    Spring MVC: Api Rest listar

--- src/main/java/com/ico/demospringguzman/models/User.java
+++ src/main/java/com/ico/demospringguzman/models/User.java
@@ -4,6 +4,14 @@ public class User {
     private String name;
     private String lastname;
 
+    public User(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public User() {
+    }
+
     public String getName() {
         return name;
     }
