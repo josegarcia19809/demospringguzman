044ed97fcc2177493cbd2cbf13fd51df2a2983e5
josegarcia19809 <josegarcia19809@gmail.com>
2024-11-24 20:11:38 -0600

    Spring MVC: Obtener varios parámetros de la ruta con @PathVariable

--- src/main/java/com/ico/demospringguzman/controllers/PathVariableController.java
+++ src/main/java/com/ico/demospringguzman/controllers/PathVariableController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
@@ -17,5 +20,15 @@ public class PathVariableController {
         paramDTO.setCode(0);
         return paramDTO;
         // http://localhost:8080/api/var/producto/pc
+    }
+
+    @GetMapping("/producto_mix/{nombre}/{id}")
+    public Map<String, Object> getProducto(@PathVariable String nombre,
+                                       @PathVariable int id) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("producto", nombre);
+        map.put("id", id);
+        return map;
+        // http://localhost:8080/api/var/producto_mix/pc/2500
     }
 }
