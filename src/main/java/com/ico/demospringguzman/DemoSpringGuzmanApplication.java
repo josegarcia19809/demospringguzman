e0d3d14bb4376520286492eebf158474002acfdc
josegarcia19809 <josegarcia19809@gmail.com>
2024-12-02 15:23:40 -0600

    Spring MVC: Agregando otros archivos properties personalizados para las configuraciones

--- src/main/java/com/ico/demospringguzman/DemoSpringGuzmanApplication.java
+++ src/main/java/com/ico/demospringguzman/DemoSpringGuzmanApplication.java
@@ -2,13 +2,19 @@ package com.ico.demospringguzman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+// @PropertySource("classpath:values.properties")
+@PropertySources({
+        @PropertySource("classpath:values.properties") // Se separan por comas
+})
 public class DemoSpringGuzmanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringGuzmanApplication.class, args);
-		System.out.println("Aplicación corriendo en puerto 8080");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringGuzmanApplication.class, args);
+        System.out.println("Aplicación corriendo en puerto 8080");
+    }
 
 }
