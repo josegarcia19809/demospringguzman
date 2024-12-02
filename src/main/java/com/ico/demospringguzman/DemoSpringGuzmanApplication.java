aa59059b8f8f32369b36fdea1d919c8bb90b9330
josegarcia19809 <josegarcia19809@gmail.com>
2024-12-02 15:32:59 -0600

    Spring MVC: Encoding charset en properties

--- src/main/java/com/ico/demospringguzman/DemoSpringGuzmanApplication.java
+++ src/main/java/com/ico/demospringguzman/DemoSpringGuzmanApplication.java
@@ -2,14 +2,9 @@ package com.ico.demospringguzman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
+
 
 @SpringBootApplication
-// @PropertySource("classpath:values.properties")
-@PropertySources({
-        @PropertySource("classpath:values.properties") // Se separan por comas
-})
 public class DemoSpringGuzmanApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/ico/demospringguzman/ValuesConfig.java
@@ -0,0 +1,13 @@
+package com.ico.demospringguzman;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+// @PropertySource("classpath:values.properties")
+@PropertySources({
+        @PropertySource(value = "classpath:values.properties", encoding = "UTF-8")
+})
+public class ValuesConfig {
+}
